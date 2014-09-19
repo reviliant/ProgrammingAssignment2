d33853a28e8ccd3d7fb28a275c557680c85db6bd
reviliant <abhigt.jumech@gmail.com>
2014-09-19 15:47:20 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## makeCacheMatrix function is used to getting/setting the value of a matrix (cached) and cacheSolve function is used to 
 # get the cached value (if it exists) or re-compute the value if the cached value does not exist
+#Call cacheSolve(<matrix : Square Invertible>) Working if you run the Code
 
 ## Write a short comment describing this function
 #The function returns a list of all the functions (set,get,setInverse and getInverse) desfined within
